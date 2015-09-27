67afc2ace1432c7fd69fea02b33ba26a04b5b9aa
Schwiti <m.schwitalla@hotmail.com>
2015-09-27 14:44:42 +0200

    new funcs

--- func/loadData.m
+++ func/loadData.m
@@ -1,7 +1,7 @@
 function [dataTable] = loadData(datCsv)
     disp('reading table from csv file..');
     dataTable = readtable(datCsv,'ReadVariableNames',false,'ReadRowNames',false,...
-    'TreatAsEmpty',{'.','NA'});
+    'TreatAsEmpty',{'NA'});
        
     cnames = {'time','bid','ask','size_bid','size_ask'};
     dataTable.Properties.VariableNames = cnames;
--- func/preprocessTable.m
+++ func/preprocessTable.m
@@ -4,11 +4,11 @@ function [preDS] = preprocessTable(dataTable)
     dataTable = dataTable(~any(TF,2),:);
     
     M = zeros(size(dataTable) + [0 1]);
-    M(:,1) = datenum(dataTable.time);
+    M(:,1) = datenum(dataTable.time,'yyyy.mm.dd HH:MM:SS.FFF');
     M(:,2:5) = dataTable{:,2:5};
     M(:,6) = dataTable.ask-dataTable.bid;
       
     preDS = dataset({M 'time','bid','ask','size_bid','size_ask','spread'});
     preDS((preDS.spread<0),:) = []; %remove negative spreads;
     disp('DONE!');
-end
+end 
