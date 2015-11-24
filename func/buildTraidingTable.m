8768f6e7557de0aa615d7c2e3601af770d2e69c2
Daniel Waelchli <whitestguyiknow@github.com>
2015-11-24 19:47:45 +0100

    debugging

--- func/buildTraidingTable.m
+++ func/buildTraidingTable.m
@@ -3,6 +3,7 @@ l = length(position);
 N = length(time);
 m = l/2;
 
+assert(mod(l,2)==0);
 assert(sum(position)==0,'position must be even out');
 assert(length(tradeTime)==l);
 assert(length(bid)==N);
@@ -10,39 +11,45 @@ assert(length(ask)==N);
 assert(length(usdKurs)==N);
 assert(mod(l,2)==0);
 
-entryTime   = zeros(m,1);
-entryPrice  = zeros(m,1);
-%position    = zeros(m,1);
-exitTime    = zeros(m,1);
-exitPrice   = zeros(m,1);
-lowPrice    = zeros(m,1);
-highPrice   = zeros(m,1);
+entryTime   = zeros(l,1);
+entryPrice  = zeros(l,1);
+entryPosition    = zeros(l,1);
+exitPosition    = zeros(l,1);
+exitTime    = zeros(l,1);
+exitPrice   = zeros(l,1);
+lowPrice    = zeros(l,1);
+highPrice   = zeros(l,1);
 
 tradeIdx = zeros(1,l);
 
+idx=1;
 for i = 1:2:l
+    
     t = tradeTime(i);
     j = find(time>=t,1,'first');
     tradeIdx(i) = j;
-    entryTime(i) = time(j);
+    entryTime(idx) = time(j);
+    entryPosition(idx) = position(i);
     if(position(i)>0)
-        entryPrice(i)=ask(j);
+        entryPrice(idx)=ask(j);
     else
-        entryPrice(i)=bid(j);
+        entryPrice(idx)=bid(j);
     end
     
     t = tradeTime(i+1);
     j = find(time>=t,1,'first');
     tradeIdx(i+1) = j;
-    exitTime(i+1) = time(j);
+    exitTime(idx) = time(j);
+    exitPosition(idx) = position(i+1);
     if(position(i+1)>0)
-        exitPrice(i+1)=ask(j);
+        exitPrice(idx)=ask(j);
     else
-        exitPrice(i+1)=bid(j);
+        exitPrice(idx)=bid(j);
     end
+    idx = idx+1;
 end
 
-bruttoPnL = (exitPrice-entryPrice).*position;
+bruttoPnL = exitPrice.*entryPosition-entryPrice.*exitPosition;
 bruttoPnLUSD = bruttoPnL.*usdKurs(tradeIdx);
 commissionUSD = abs(position).*comission;
 nettoPnLUSD = bruttoPnLUSD - comissionUSD;
