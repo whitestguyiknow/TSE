e1f7025c8bffd13e70e53158c4b8c28058fb64ff
Daniel Waelchli <wadaniel@student.ethz.ch>
2016-01-15 02:58:43 +0100

    func appendIndicator

--- func/appendIndicator.m
+++ func/appendIndicator.m
@@ -1,4 +1,8 @@
-function [DS] = appendIndicator(DS,name,f)
-    
+function [DS] = appendIndicator(DS,f,name)
+    assert(strcmp(class(f),'function_handle'), 'f must be a function handle');
+    assert(strcmp(class(f),'dataset'), 'DS must be a dataset');
+    assert(ischar(name), 'name must be a string');
+    DS.indicator = f(DS);
+    DS.Properties.VarNames(end)={name};
 end
 
