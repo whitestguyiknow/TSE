65b7bf12bf7dedc59c9f2bd25e98ff6e6479c3dd
Daniel Waelchli <wadaniel@student.ethz.ch>
2016-01-27 18:58:57 +0100

    buildDailyTradingTable corrected min size

--- func/buildDailyTradingTable.m
+++ func/buildDailyTradingTable.m
@@ -3,10 +3,11 @@ function [dailyTradingTable] = buildDailyTradingTable(tradingTable)
 disp('building daily trading table..');
 tic;
 
-if(isempty(tradingTable))
+if(isempty(tradingTable) || height(tradingTable)<10) % 10 min trades for now..
     dailyTradingTable = [];
     return 
 end
+
 tradingIntervall = tradingTable.ExitTime(end,:) - tradingTable.EntryTime(1,:);
 M = 365*tradingIntervall(1)+30*tradingIntervall(2)+tradingIntervall(3);
 
