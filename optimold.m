7bbe65d27680b65eeba05602cd25dfc05b45ae39
Schwiti <m.schwitalla@hotmail.com>
2016-01-31 22:26:42 +0100

    oldoptim

---
+++ optimold.m
@@ -0,0 +1,43 @@
+function [sharpe] = optimold(DSpre,DS1,DS2,deltaRSI)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Function: exanple function feeding into DEoptim           
+% created by Daniel, December 2015
+%
+% Last update: 2016 Jan 31, by Daniel
+%   2016-01-31: (Daniel)
+%       1. restructuring
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% warm up time
+tInit = 100;
+equityInit = 100000;
+
+% artificial exchange rate
+usdkurs = ones(length(DSpre.time),1);
+comission = 0.5*8/100000;
+
+%% SECTION TO INTERCHANGE INDICATORS
+% function handles to indicators
+fBuyEntry = @(DS1,i,DS2,k,DS3,l) entryBuyRSI(DS1,i,DS2,k,deltaRSI);
+fSellEntry = @(DS1,i,DS2,k,DS3,l) entrySellRSI(DS1,i,DS2,k,deltaRSI);
+fBuyExit = @(DS1,i,DS2,k,DS3,l) exitBuyTrailingSDEV(DS1,i,DS2,k);
+fSellExit = @(DS1,i,DS2,k,DS3,l) exitSellTrailingSDEV(DS1,i,DS2,k);
+%%
+
+% initialize global indicator struct
+setIndicatorStruct();
+
+% action matrix and time
+[Time, Action] = buildActionMatrix(DS1,DS2,DS2,tInit,fBuyEntry,fBuyExit,fSellEntry,fSellExit);
+
+% generate trades
+tradingTable = buildTradingTable(DSpre, equityInit, usdkurs,comission,Time,Action*100000);
+
+% summary
+dailyTT = buildDailyTradingTable(tradingTable, equityInit);
+
+% sharpe-ratio
+sharpe = sharpeRatio(dailyTT);
+end
+
