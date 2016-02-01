eb7c8526c5c21eb2366abd9f07901bf9d062c9ed
Daniel Waelchli <wadaniel@student.ethz.ch>
2016-02-01 01:50:51 +0100

    ..

--- DE/generateOptimStruct.m
+++ DE/generateOptimStruct.m
@@ -2,7 +2,6 @@ function [S] = generateOptimStruct(nAgents,maxIter,CR,F,seed,N_cpu)
     assert(CR>0 && CR<1,'crossover probability must be in (0,1)')
     assert(F>0 && F<2,'differential weight must be in (0,2)')
     assert(N_cpu>0,' number od cpu mus be >0')
-    assert(inSam>0,' number od cpu mus be >0')
     
     S = struct('nAgents',nAgents,'maxIter',maxIter,'crossOverProbability',CR,...
         'differentialWeight',F,'seed',seed,'N_cpu',N_cpu);
