90d18035947c5a09691cb5c51f3b0238f36cbed3
Daniel Waelchli <wadaniel@student.ethz.ch>
2016-03-10 02:10:55 +0100

    added trycatch in stoch indic., fast solution

--- indicators/entrySellStoch.m
+++ indicators/entrySellStoch.m
@@ -1,18 +1,23 @@
 function [b] = entrySellStoch(DS1,i,DS2,k,LB,x)
-T = round(x);
 
-L1 = min(DS1.LOW_ask(i-T:i));
-H1 = max(DS1.HIGH_ask(i-T:i));
-stoch_X1 = (DS1.ask_close(i)-L1)/(H1-L1);
+try
+    T = round(x);
 
-L2 = min(DS1.LOW_ask(i-LB-T:i-LB));
-H2 = max(DS1.HIGH_ask(i-LB-T:i-LB));
-stoch_X2 = (DS1.ask_close(i)-L2)/(H2-L2);
+    L1 = min(DS1.LOW_ask(i-T:i));
+    H1 = max(DS1.HIGH_ask(i-T:i));
+    stoch_X1 = (DS1.ask_close(i)-L1)/(H1-L1);
 
-if isempty(L1) || isempty(L2)
-    b=false;
-else
-    b=(stoch_X1<0.4 && stoch_X2>0.4);
+    L2 = min(DS1.LOW_ask(i-LB-T:i-LB));
+    H2 = max(DS1.HIGH_ask(i-LB-T:i-LB));
+    stoch_X2 = (DS1.ask_close(i)-L2)/(H2-L2);
+
+    if isempty(L1) || isempty(L2)
+        b=false;
+    else
+        b=(stoch_X1<0.4 && stoch_X2>0.4);
+    end
+catch
+    b = false;
 end
 
 end
