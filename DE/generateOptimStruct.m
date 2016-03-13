56e48e8d2fc14e7b7fb9702e7e4f0212798581af
Daniel Waelchli <wadaniel@student.ethz.ch>
2016-03-13 21:46:07 +0100

    data

--- DE/generateOptimStruct.m
+++ DE/generateOptimStruct.m
@@ -1,10 +1,10 @@
 function [S] = generateOptimStruct()
-S.enable_parallel = false;
+S.enable_parallel = true;
 S.nPopulation = 30;
 S.maxIter = 100;
 S.CR = 0.7;
 S.F = 1.5;
-S.N_cpu = 1;
+S.N_cpu = 2;
 S.seed = 8392;
 end
 
